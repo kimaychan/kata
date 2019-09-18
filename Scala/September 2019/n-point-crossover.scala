fe4961f66ee57942efdcde9d0fb425e22c8b234b
Mikee Rose I. Bersabal <mribersabal@vauldex.com>
2019-09-18 14:25:33 +0800

    Update kata

--- Scala/September 2019/n-point-crossover.scala
+++ Scala/September 2019/n-point-crossover.scala
@@ -1,3 +1,14 @@
+/*
+  In genetic algorithms, a crossover allows 2 chromosomes to exchange part of their genes.
+
+  For more details, you can visit these wikipedia links : Genetic algorithm and Crossover.
+
+  A chromosome is represented by a list of genes.
+  Consider for instance 2 chromosomes xs (with genes [x,x,x,x,x,x]) and ys (with genes [y,y,y,y,y,y])
+
+  We want to generalize this to the notion of n-point crossover, and create a generic function.
+*/
+
 object NPointCrossover extends App {
   def crossover[T](ns: List[Int], xs: List[T], ys: List[T]): (List[T], List[T]) = {
     def aux(x: List[T], y: List[T], isCross: Boolean = false, i: Int = 1, acc: (List[T], List[T]) = (List(), List())): (List[T], List[T]) = {
@@ -7,3 +18,5 @@ object NPointCrossover extends App {
     aux(xs, ys, ns.contains(0))
   }
 }
+
+// 09-18-2019
