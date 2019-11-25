e20eb7c0c8daa33561859ef28094f7d951edd345
Mikee Rose I. Bersabal <mribersabal@vauldex.com>
2019-11-25 11:30:04 +0800

    Add Kata

---
+++ Javascript/November 2019/is-happy-number.js
@@ -0,0 +1,27 @@
+/**
+ * Write an algorithm to determine if a number is "happy". 
+ * A happy number is a number defined by the following process: Starting with any positive integer, 
+ * replace the number by the sum of the squares of its digits, and repeat the process until the 
+ * number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. 
+ * Those numbers for which this process ends in 1 are happy numbers.
+ * 
+ * 
+ * @param {number} n
+ * @return {boolean}
+ */
+
+var isHappy = function(n, cycle) {
+  cycle = cycle || []
+  cycle.push(n)
+  
+  if (n < 0) return false
+  
+  const sum = n.toString()
+    .split("")
+    .reduce((res, val) => res + (val ** 2) , 0)
+
+  if (sum == 1) return true
+  if (cycle.includes(sum)) return false
+  
+  return isHappy(sum, cycle)
+};
