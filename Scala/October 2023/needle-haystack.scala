1745a17a3acd44dd5b2d3f2818ee91b4c6c9a521
kimaychan <mikeebersabal@gmail.com>
2023-10-21 16:15:14 +0800

    add details

--- Scala/October 2023/needle-haystack.scala
+++ Scala/October 2023/needle-haystack.scala
@@ -1,3 +1,11 @@
+/**
+  * Can you find the needle in the haystack?
+  * Write a function findNeedle() that takes an array full of junk but containing one "needle"
+  * After your function finds the needle it should return a message (as a string) that says: "found the needle at position " plus the index it found the needle, so:
+    Example(Input --> Output)
+    ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+  */
+
 def findNeedle(haystack: List[String]): String = {
   val index = haystack.indexWhere(_ == "needle")
   s"found the needle at position $index"
